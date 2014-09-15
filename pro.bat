246efd476f9d7be0fe08652c2498f664e0befbe2
Mohankumar8 <mohan.k92@gmail.com>
2014-09-15 17:51:40 +0530

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,6 +1,6 @@
 cd C:\Users\The One\.jenkins\jobs\Android application\workspace\Pro\bin
-adb -s Nexus7 install Pro.apk
+adb -r install Pro.apk
 cd C:\Users\The One\.jenkins\jobs\Android application\workspace\ProTest\bin
-adb -s Nexus7 install ProTest.apk
-start adb -s Nexus7 shell am instrument -w com.example.pro.test/android.test.InstrumentationTestRunner
+adb -r install ProTest.apk
+start adb shell am instrument -w com.example.pro.test/android.test.InstrumentationTestRunner
 exit
