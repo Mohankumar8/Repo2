788d6ff203a391aaaf0b2da649a653d2a4d7050c
Mohankumar8 <mohan.k92@gmail.com>
2014-09-15 12:18:22 +0530

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,7 +1,6 @@
 cd C:\Mohan\One\adt-bundle\sdk\tools
 emulator -avd Nexus7
 emulator -avd Nexus5
-pause
 cd C:\Mohan\One\workspace3\Pro\bin
 adb -s emulator-5554 install Pro.apk
 adb -s emulator-5556 install Pro.apk
