32fea0cfb5ffa9ca32aa49248d863893189e620d
Mohankumar8 <mohan.k92@gmail.com>
2014-09-15 17:41:34 +0530

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,10 +1,7 @@
-cd C:\Mohan\One\adt-bundle\sdk\tools
-emulator -avd Nexus7
-emulator -avd Nexus5
-cd Pro\bin
+cd C:\Users\The One\.jenkins\jobs\Android application\workspace\Pro\bin
 adb -s emulator-5554 install Pro.apk
 adb -s emulator-5556 install Pro.apk
-cd ProTest\bin
+cd C:\Users\The One\.jenkins\jobs\Android application\workspace\ProTest\bin
 adb -s emulator-5554 install ProTest.apk
 adb -s emulator-5556 install ProTest.apk
 start adb -s emulator-5554 shell am instrument -w com.example.pro.test/android.test.InstrumentationTestRunner
