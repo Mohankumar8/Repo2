734b00bfb400ba2434cb64835d885ab072d3de1d
Mohankumar8 <mohan.k92@gmail.com>
2014-09-16 12:39:03 +0530

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,6 +1,6 @@
-cd C:\Users\The One\.jenkins\jobs\Android application\workspace\Pro\bin
+cd C:\Program Files (x86)\Jenkins\workspace\Job3\Pro\bin
 adb -r install Pro.apk
-cd C:\Users\The One\.jenkins\jobs\Android application\workspace\ProTest\bin
+cd C:\Program Files (x86)\Jenkins\workspace\Job3\ProTest\bin
 adb -r install ProTest.apk
 start adb shell am instrument -w com.example.pro.test/android.test.InstrumentationTestRunner
 
